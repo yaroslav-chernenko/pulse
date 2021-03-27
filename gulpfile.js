5aabe7666510cf513f8c6fefea6a2d43c7872c98
Yaroslav <yarik1121@gmail.com>
2021-03-27 19:21:49 +0200

    add first screen

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('server', function () {
 });
 
 gulp.task('styles', function () {
-   return gulp.src("src/scss/**/*.+(scss|sass)")
+   return gulp.src("src/sass/**/*.+(scss|sass)")
       .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
       .pipe(rename({ suffix: '.min', prefix: '' }))
       .pipe(autoprefixer())
